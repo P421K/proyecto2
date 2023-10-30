392016f5ce35ab80a85be75db01786bbb2366310
P421K <parik4@gmail.com>
2023-10-30 09:22:29 +0100

    configurado btn apagar, corrigiendo btn apagado para que no funcionen otros btn

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ let btnLeft = document.getElementById("btn-left");
 let btnRight = document.getElementById("btn-right");
 let displayChange = document.getElementById("displayChange");
 let contador = 0;
-let luzEncendido = 0;
+let luzEncendido = 1;
+let power = document.getElementById("btn-power");
 let powerOn = document.getElementById("light-power-on");
 let powerOff = document.getElementById("light-power-off");
 
@@ -17,50 +18,59 @@ let powerOff = document.getElementById("light-power-off");
 //   displayChange.src = "./img/imgGames/img-pacMan.jpeg";
 // };
 
-//accion del boton right
-btnRight.onclick = function () {
-  if (contador == 0) {
-    displayChange.src = "./img/imgGames/img-sims.jpeg";
-    contador++;
-    console.log(`contador en ${contador}`);
-  } else if (contador == 1) {
-    displayChange.src = "./img/imgGames/img-pacMan.jpeg";
-    contador++;
-    console.log(`contador en ${contador}`);
-  } else if (contador == 2) {
-    displayChange.src = "./img/imgGames/img-topGear.jpeg";
-    contador++;
-    console.log(`contador en ${contador}`);
-  } else if (contador == 3) {
-    displayChange.src = "./img/imgGames/img-princeOfPersia.jpeg";
-    contador++;
-    console.log(`contador en ${contador}`);
+//acciones del boton power
+power.onclick = function () {
+  if (luzEncendido == 0) {
+    powerOff.style.backgroundColor = "#ff0000";
+    displayChange.src = "./img/imgGames/img-inicio.jpeg";
+    luzEncendido++;
+    console.log("apagado0");
+  } else if (luzEncendido == 1) {
+    powerOff.style.backgroundColor = "#008000";
+    displayChange.src = "./img/imgGames/img-batman.jpeg";
+    luzEncendido--;
+    console.log("apagado1");
+
+    //accion del boton right
+    btnRight.onclick = function () {
+      if (contador <= 0) {
+        displayChange.src = "./img/imgGames/img-sims.jpeg";
+        contador++;
+        console.log(`contador en ${contador}`);
+      } else if (contador == 1) {
+        displayChange.src = "./img/imgGames/img-pacMan.jpeg";
+        contador++;
+        console.log(`contador en ${contador}`);
+      } else if (contador == 2) {
+        displayChange.src = "./img/imgGames/img-topGear.jpeg";
+        contador++;
+        console.log(`contador en ${contador}`);
+      } else if (contador >= 3) {
+        displayChange.src = "./img/imgGames/img-princeOfPersia.jpeg";
+        contador++;
+        console.log(`contador en ${contador}`);
+      }
+    };
+
+    //accion del boton left
+    btnLeft.onclick = function () {
+      if (contador <= 0) {
+        displayChange.src = "./img/imgGames/img-sims.jpeg";
+        contador--;
+        console.log(`contador en ${contador}`);
+      } else if (contador == 1) {
+        displayChange.src = "./img/imgGames/img-pacMan.jpeg";
+        contador--;
+        console.log(`contador en ${contador}`);
+      } else if (contador == 2) {
+        displayChange.src = "./img/imgGames/img-topGear.jpeg";
+        contador--;
+        console.log(`contador en ${contador}`);
+      } else if (contador >= 3) {
+        displayChange.src = "./img/imgGames/img-princeOfPersia.jpeg";
+        contador--;
+        console.log(`contador en ${contador}`);
+      }
+    };
   }
 };
-
-//accion del boton left
-btnLeft.onclick = function () {
-  if (contador == 0) {
-    displayChange.src = "./img/imgGames/img-sims.jpeg";
-    contador--;
-    console.log(`contador en ${contador}`);
-  } else if (contador == 1) {
-    displayChange.src = "./img/imgGames/img-pacMan.jpeg";
-    contador--;
-    console.log(`contador en ${contador}`);
-  } else if (contador == 2) {
-    displayChange.src = "./img/imgGames/img-topGear.jpeg";
-    contador--;
-    console.log(`contador en ${contador}`);
-  } else if (contador == 3) {
-    displayChange.src = "./img/imgGames/img-princeOfPersia.jpeg";
-    contador--;
-    console.log(`contador en ${contador}`);
-  }
-};
-
-luzEncendido.onclick = function(){
-  if (luzEncendido == 0){
-    
-  }
-}
